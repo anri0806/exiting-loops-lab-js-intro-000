b3268f12c19dc262f4bd00a57365f395da67da48
Anri Kawahara <anri0806@hotmail.co.jp>
2021-05-17 19:51:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+   array[i] = changeValue;
+   if (array[i] !== stopValue) {
+    break
+     return array[i]
+  }
+  }
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = changeValue
+    if (array[i] === skipValue) {
+      continue
+    }
+       return array
+  }
+}
