60c1c4b62d6b35c1c1a712d63210608a01600122
Anri Kawahara <anri0806@hotmail.co.jp>
2021-05-17 20:12:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0; i < array.length, i++){
-
+    if (findFn === 0) {
+      return null
+    }
     
   }
 }
